6e2f1c3595283aa57426db165bd1a03d7f2dad0a
guilherme8l <101971954+Ilhe8l@users.noreply.github.com>
2024-04-26 11:27:57 -0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,6 +1,8 @@
 const sign_in_btn = document.querySelector("#sign-in-btn"),
     sign_up_btn = document.querySelector("#sign-up-btn"),
-    container = document.querySelector(".container");
+    container = document.querySelector(".container"),
+    sign_in_btn2 = document.querySelector("#sign-in-btn2"),
+    sign_up_btn2 = document.querySelector("#sign-up-btn2");
 
 sign_up_btn.addEventListener("click", () =>{
     container.classList.add("sign-up-mode");
@@ -10,3 +12,10 @@ sign_in_btn.addEventListener("click", () =>{
     container.classList.remove("sign-up-mode");
 });
 
+sign_up_btn2.addEventListener("click", () =>{
+    container.classList.add("sign-up-mode2");
+});
+
+sign_in_btn2.addEventListener("click", () =>{
+    container.classList.remove("sign-up-mode2");
+});
